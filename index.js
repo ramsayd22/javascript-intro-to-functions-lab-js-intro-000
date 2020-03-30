bbede3b5e4227f1174e544519f94e427dd5d6e3e
Darnell Ramsay <darnell.ramsay@gmail.com>
2020-03-30 02:09:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout (aWord) {
 function logWhisper (aWord) {
   console.log(aWord.toLowerCase( ));
 }
+function sayHiToGrandma (aWord) {
+  var cantUnswer = "I can't hear you!";
+  var yesUnswer = "YES INDEED!";
+  var lovUnswer = "I love you, too.";
+  if (aWord.toLowerCase(aWord) === aWord) {
+    return cantUnswer;
+  }
+  else if (aWord.toUpperCase(aWord) === aWord) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === aWord) {
+    return lovUnswer
+  }
+}
